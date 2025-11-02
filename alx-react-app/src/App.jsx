29a8019c54f46e18f665f03fcc76f3f3d2579d17
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-02 14:56:04 +0000

    v1.5

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -12,8 +12,10 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
+    
     <>
      <WelcomeMessage />
+     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
      
      <Header />
      
