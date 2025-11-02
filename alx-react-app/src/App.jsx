b934c33295fbbf23e3df77c77c00f16aad0008c4
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-02 13:15:21 +0000

    v1.3

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,11 +5,12 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-   <WelcomeMessage />
+  
   const [count, setCount] = useState(0)
 
   return (
     <>
+     <WelcomeMessage />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
