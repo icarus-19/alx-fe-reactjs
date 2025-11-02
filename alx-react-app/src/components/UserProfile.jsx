a8cf8580300dabccf6f4f6122080c7f5bc7bb05d
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-02 17:48:12 +0000

    v1.7

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+ import React from 'react';
+
+const UserProfile = ({ name, age, bio }) => {
+  return (
+    <div className="user-profile">
+      <h2>{name}</h2>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
+    </div>
+  );
+};
+
+export default UserProfile;
