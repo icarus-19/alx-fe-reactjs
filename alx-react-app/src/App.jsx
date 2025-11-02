1fa1da365f7be907cf163000618dc6ac897cc0b1
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-02 07:44:15 +0000

    v1.2

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,9 +1,11 @@
+import WelcomeMessage from './components/WelcomeMessage'
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+   <WelcomeMessage />
   const [count, setCount] = useState(0)
 
   return (
