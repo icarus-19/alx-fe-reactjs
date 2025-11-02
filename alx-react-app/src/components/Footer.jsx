71856bfe81023be486a318b9253718b1c19ef67f
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-02 17:39:57 +0000

    v1.6

--- alx-react-app/src/components/Footer.jsx
+++ alx-react-app/src/components/Footer.jsx
@@ -1,3 +1,11 @@
-<footer>
-  <p>© 2023 City Lovers</p>
-</footer>
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>© 2023 City Lovers</p>
+    </footer>
+  );
+};
+
+export default Footer;
