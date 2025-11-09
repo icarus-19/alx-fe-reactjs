fad912c4aa86266e8bf17523997e88e103e0ce8a
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-09 08:25:51 +0000

    v1.6.4.1

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -9,6 +9,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Counter from './Counter'
+
 function App() {
   
   const [count, setCount] = useState(0)
@@ -47,5 +49,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
