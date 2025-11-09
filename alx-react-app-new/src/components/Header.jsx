7a3f71ac9e3b127fb1b8feffcdaf27c19e945861
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-09 03:37:00 +0000

    v1.6.1

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+<header style={{ backgroundColor: 'navy', color: 'white', textAlign: 'center' }}>
+   <h1>My Favorite Cities</h1>
+ </header>
+
+const FavoriteCitiesHeader = () => {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+export default FavoriteCitiesHeader;
+
+<header>
+  <h1>My Favorite Cities</h1>
+</header>
