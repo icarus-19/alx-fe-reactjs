5ee5bf59dbedf639c4becd24437c34c73ff90c8d
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-09 08:37:05 +0000

    v1.8

---
+++ alx-react-app-prop/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const FavoriteCitiesHeader = () => {
+  return (
+    <header>
+      <h1>My Favorite Cities</h1>
+    </header>
+  );
+};
+
+export default FavoriteCitiesHeader;
+
+<header>
+  <h1>My Favorite Cities</h1>
+</header>
