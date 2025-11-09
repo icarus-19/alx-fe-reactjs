69d01c4f203d10abbd21578549e41bc6aba9302d
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-09 16:54:24 +0000

    rv1.9.3

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -9,6 +9,28 @@ import About from './components/About'
 import Contact from './components/Contact'
 import './App.css'
 
+import { Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </main>
+    </div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
