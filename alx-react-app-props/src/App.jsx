34e10a22b0949d99b00a21f39cd59db565a82056
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-09 08:45:55 +0000

    v1.8.2

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -9,6 +9,17 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import ProfilePage from './ProfilePage';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
+}
+
+
+
+
 function App() {
   
   const [count, setCount] = useState(0)
