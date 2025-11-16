936bd27baef7abef7616be92ade4d770db8279a4
icarus-19 <119785560+icarus-19@users.noreply.github.com>
2025-11-16 14:49:31 +0000

    v.2.2

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import create from 'zustand'
+
+const useRecipeStore = create(set => ({
+  recipes: [],
+  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes })
+}));
+
 function App() {
   const [count, setCount] = useState(0)
 
